8892c82bcd186f443bb8c8cd4790897f3db7d2d3
Zifeng <353283080@qq.com>
2021-01-31 02:49:53 -0500

    add mainpage UI layout

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from login.models import User
 
+
 # Create your views here.
 def main_page(request, user_id):
     # redirect to login if try to access directly
@@ -17,6 +18,8 @@ def main_page(request, user_id):
         return render(request, 'mainpage/main.html', context)
 
 
+# Listen on main page buttons and redirect them to different major functionalities
+# passing user id to next pages
 def direct_func_buttons(request, user_id):
     try:
         user = User.objects.get(pk=user_id)
@@ -24,11 +27,11 @@ def direct_func_buttons(request, user_id):
         return render(request, 'login/index.html', {})
     else:
         if request.GET.get('my_ride_btn'):
-            return HttpResponseRedirect(reverse('my_ride:my_ride', args = (user_id,)))
+            return HttpResponseRedirect(reverse('my_ride:my_ride', args=(user_id,)))
         elif request.GET.get('request_ride_btn'):
-            return HttpResponseRedirect(reverse('request_ride:request_ride', args = (user_id,)))
+            return HttpResponseRedirect(reverse('request_ride:request_ride', args=(user_id,)))
         elif request.GET.get('join_ride_btn'):
-            return HttpResponseRedirect(reverse('join_ride:join_ride', args = (user_id,)))
+            return HttpResponseRedirect(reverse('join_ride:join_ride', args=(user_id,)))
         elif request.GET.get('driver_btn'):
-            return HttpResponseRedirect(reverse('driver_access:driver_access', args = (user_id,)))
+            return HttpResponseRedirect(reverse('driver_access:driver_access', args=(user_id,)))
 
