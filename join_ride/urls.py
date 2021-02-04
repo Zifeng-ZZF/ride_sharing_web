82b0a5b3d3be4561e36c226fef2d212dd8f0ddd1
“YifanShao” <yifan.shao@duke.edu>
2021-02-04 02:23:13 -0500

    yifan

--- join_ride/urls.py
+++ join_ride/urls.py
@@ -5,5 +5,5 @@ app_name = 'join_ride'
 urlpatterns = [
     path('', views.join_ride, name='join_ride'),
     path('search/', views.search, name='search'),
-    path('<int:ride_id>/request_process/', views.request_process, name='request_process'),
+    path('<int:ride_id>/<int:p_num>/request_process/', views.request_process, name='request_process'),
 ]
--- join_ride/views.py
+++ join_ride/views.py
@@ -8,7 +8,14 @@ from django.urls import reverse
 from django.utils import dateparse
 from django.db.models import Q
 
+class RequestInfo:
+   searched_rides = None
+   total_passenger_num = 0
+   def __init__(self, searched_rides, total_passenger_num):
+      self.searched_rides = searched_rides
+      self.total_passenger_num = total_passenger_num
 # Create your views here.
+@login_required(login_url='login:index')
 def join_ride(request):
     return render(request, 'join_ride/join_ride.html', {'list':list})
 
@@ -25,17 +32,18 @@ def search(request):
             all_rides_list = Ride.objects.filter(Q(owner = request.user) | Q(sharer__has_any_keys=[request.user.username]))
             #rides, which user is already in, should not be displayed
             ride = ride.exclude(id__in=all_rides_list)
-            return render(request, 'join_ride/search_result.html', {'ans':ride})
+            
+            return render(request, 'join_ride/search_result.html', {'ans':ride, 'p_num':total_passenger_num})
 
 #process the join request. 
 #Add username into Ride.sharer, ++passengernum
 @login_required(login_url='login:index')
-def request_process(request, ride_id):
+def request_process(request, ride_id, p_num):
     ride = Ride.objects.get(id = ride_id)
     sharer = ride.sharer
     sharer.append(request.user.username)
     ride.sharer = sharer
-    ride.total_passenger_num = ride.total_passenger_num + 1
+    ride.total_passenger_num = ride.total_passenger_num + p_num
     ride.save()
     return render(request, 'mainpage/main.html')
 
